22c394b187e8eee8276f21acb9efe02c64ec1bc9
Muresan Andrei <muresan.1andrei@gmail.com>
2017-02-07 15:44:32 +0200

    merge on opencart

--- README.md
+++
@@ -1,3 +0,0 @@
-git remote add origin git@github.com:muresanandrei1/toner.git
-
-git pull
--- index.php
+++ index.php
@@ -1,17 +1,19 @@
 <?php
-/**
- * Front to the WordPress application. This file doesn't do anything, but loads
- * wp-blog-header.php which does and tells WordPress to load the theme.
- *
- * @package WordPress
- */
+// Version
+define('VERSION', '2.3.0.2');
 
-/**
- * Tells WordPress to load the WordPress theme and output it.
- *
- * @var bool
- */
-define('WP_USE_THEMES', true);
+// Configuration
+if (is_file('config.php')) {
+	require_once('config.php');
+}
 
-/** Loads the WordPress Environment and Template */
-require( dirname( __FILE__ ) . '/wp-blog-header.php' );
+// Install
+if (!defined('DIR_APPLICATION')) {
+	header('Location: install/index.php');
+	exit;
+}
+
+// Startup
+require_once(DIR_SYSTEM . 'startup.php');
+
+start('catalog');
